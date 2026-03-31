e5b3c986db9a9aff73d382ce3d8d4a2e74f1a96e
Minsuk Shin <elgd00@gmail.com>
2026-03-31 17:37:53 +0900

    feat: snap shot sync

--- sync.ps1
+++ sync.ps1
@@ -1,4 +1,11 @@
-if (-not (git diff --quiet) -or -not (git diff --cached --quiet)) {
+
+git diff --quiet
+$dirty1 = $LASTEXITCODE
+
+git diff --cached --quiet
+$dirty2 = $LASTEXITCODE
+
+if ($dirty1 -ne 0 -or $dirty2 -ne 0) {
   Write-Host "Uncommitted changes detected. Please commit first."
   exit 1
 }
